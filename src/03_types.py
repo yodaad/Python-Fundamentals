44937b410311bee5169d92aa27588aeb6dc9d117
Diego Arango <yodaad@gmail.com>
2024-02-27 06:08:27 -0500

    Types file added

---
+++ src/03_types.py
@@ -0,0 +1,24 @@
+# String
+my_name = "Diego"
+my_name = 'David'
+print(my_name)
+print(type(my_name))
+
+# Int
+my_age = 39
+print(my_age)
+print(type(my_age))
+
+# Boolean
+is_single = True
+print(is_single)
+print(type(is_single))
+
+my_age = input("What's your age? ")
+print(my_age)
+print(type(my_age))
+
+# Cast wint int() function
+my_age = int(input("What's your age? "))
+print(my_age)
+print(type(my_age))
