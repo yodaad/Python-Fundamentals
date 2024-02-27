8704f2b2969c6cae647e406997d35c55c3ad22d4
Diego Arango <yodaad@gmail.com>
2024-02-27 06:05:11 -0500

    First program file added

---
+++ src/01_first_program.py
@@ -0,0 +1,17 @@
+print("Hello, this is Python.")
+
+print("Hello, I am Diego.")
+
+print(12 + 5)
+print(10 - 5)
+print(10 * 7)
+print(10 / 5)
+
+# This is a comment
+
+""" 
+This a long comment
+with multiple 
+lines
+"""
+
