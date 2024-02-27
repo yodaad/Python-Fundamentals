264e34bf11c0698ac8251af5f08f479fab444a18
Diego Arango <yodaad@gmail.com>
2024-02-27 06:07:45 -0500

    Variables file added

---
+++ src/02_variables.py
@@ -0,0 +1,12 @@
+print("Hello variables")
+my_name = "Diego"
+print(my_name)
+
+my_age = 39
+print(my_age)
+
+my_name = "Andres"
+print("In here I changed the value of the variable my_name to: ", my_name)
+
+my_name = input("Enter your name: ")
+print("Hello ", my_name)
