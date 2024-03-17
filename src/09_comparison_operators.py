4897bca435f18274c49d074941273604e889d7c3
Diego Arango <yodaad@gmail.com>
2024-03-17 09:49:01 -0500

    Comparison Operators file added

---
+++ src/09_comparison_operators.py
@@ -0,0 +1,43 @@
+# Greater than: >
+print(9 > 3)
+print(3 > 7)
+
+# Less than: <
+print(5 < 6)
+print(9 < 6)
+
+# Greater than or equal to: >=
+print(6 >= 5)
+print(6 >= 6)
+print(6 >= 7)
+
+# Less than or equal to: <=
+print(9 <= 13)
+print(9 <= 9)
+print(9 <= 8)
+
+# Equal to: ==
+print(6 == 6)
+print(6 == 5)
+print("Diego" == "Diego")
+print("Diego" == "diego")
+
+# Not equal to: !=
+print(9 != 6)
+print(9 != 9)
+print("Diego" != "Diego")
+print("Diego" != "diego")
+
+# Compare numbers
+age = 18
+print(age >= 18)
+
+# Compare strings
+obj1 = "abcd"
+obj2 = "abcde"
+
+# Compare the length of the strings
+print(len(obj1) == len(obj2))
+print(len(obj1) != len(obj2))
+print(len(obj1) > len(obj2))
+print(len(obj1) < len(obj2))
