0247ee11e672864921ff335fd91fc3cb320b8e32
Diego Arango <yodaad@gmail.com>
2024-03-17 09:41:38 -0500

    Arithmetic Operatiors file added

---
+++ src/08_arithmetic_operators.py
@@ -0,0 +1,30 @@
+# Addition
+print(10 + 10)
+
+# Substraction
+print(10 - 5)
+
+# Multiplication
+print(10 * 2)
+
+# Division
+print(10 / 3)
+
+# Integer division
+print(10 // 3)
+
+# Modulus
+print(10 % 2)
+print(10 % 3)
+
+# Exponentiation
+print(2 ** 3)
+
+# Order of operations
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+# Concatenation
+print("Hello" + " there")
+
+# Repetition
+print("Hello " * 3)
