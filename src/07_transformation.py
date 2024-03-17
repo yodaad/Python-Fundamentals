d7d890791a48f3751eaa960922c0b962cebdbe03
Diego Arango <yodaad@gmail.com>
2024-03-17 09:30:01 -0500

    Transformation file added

---
+++ src/07_transformation.py
@@ -0,0 +1,26 @@
+name = "Diego"
+print(type(name))
+
+name = 12
+print(type(name))
+
+name = True
+print(type(name))
+
+print ("Diego " + "Arango")
+print(10 + 20)
+
+age = 40
+print ("My age is " + str(age))
+print(f"My age is {age}")
+
+age = input("Enter your age: ")
+print(f"Your current age is {age}")
+age = int(age)
+age += 10
+print(f"Your age in 10 years will be: {age}")
+
+age = int(input("Enter your age: "))
+print(f"Your current age is {age}" )
+age += 10
+print(f"Your age in 10 years will be: {age}")
