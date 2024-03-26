11b959066cc02202b639d31022b6a6c004281741
Diego Arango <yodaad@gmail.com>
2024-03-26 05:51:05 -0500

    Indexing and Slicing file added

---
+++ src/15_indexing_slicing.py
@@ -0,0 +1,14 @@
+# Indexing, is the way to access the elements of a sequence like a string, list, tuple, etc.
+text = "I am reviewing Python"
+print(text[3])
+print(text[len(text) - 1])
+print(text[-1])
+
+# Slicing. is the way to access a range of elements of a sequence like a string, list, tuple, etc.
+print(text[0:12])
+print(text[:12])
+print(text[7:])
+print(text[:])
+print(text[0:15:1])
+print(text[0:15:2])
+print(text[::2])
