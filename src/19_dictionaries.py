1a74e0e394a088e8f6f12f058564eee6918a90e1
Diego Arango <yodaad@gmail.com>
2024-03-27 06:06:27 -0500

    Dictionaries file added

---
+++ src/19_dictionaries.py
@@ -0,0 +1,23 @@
+"""
+Dictionaries are a collection of key-value pairs. They are unordered, changeable and indexed. They are written with curly brackets and have keys and values separated by a colon. 
+The keys must be unique and immutable. The values can be of any data type. You can access the values by using the key name. You can also use the get() method to access the values. 
+You can change the values by using the key name. You can loop through the dictionary using a for loop. You can also use the in keyword to check if a key exists in the dictionary. 
+You can also use the len() function to get the number of key-value pairs in the dictionary.
+"""
+
+my_dictionary = {
+    "name": "Diego",
+    "last_name": "Arango",
+    "age": 39
+}
+
+print(my_dictionary)
+print(len(my_dictionary))
+
+print(my_dictionary["age"])
+print(my_dictionary["last_name"])
+print(my_dictionary.get("age"))
+print(my_dictionary.get("height"))
+
+print("age" in my_dictionary)
+print("height" in my_dictionary)
