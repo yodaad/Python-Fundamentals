2394bb38f8cc0ecea60e11de4d31bcc34be51a06
Diego Arango <yodaad@gmail.com>
2024-03-27 06:25:12 -0500

    Dictionary modification file updated

--- src/20_dictionary_modification.py
+++ src/20_dictionary_modification.py
@@ -9,21 +9,33 @@ person = {
 
 print(person)
 
+# Modify values in a dictionary
 person["name"] = "Andres"
 person["age"] -= 1
 person["languages"].append("Java")
 print(person)
 
+# Del removes the key and value from the dictionary
 del person["last_name"]
+
+# Pop removes the key and value from the dictionary as well
 person.pop("age")
 print(person)
 
+# Add a new key-value pair to the dictionary using the key name and the assignment operator
 person["last_name"] = "Arango"
 person["age"] = 40
 
+# Items returns a list of tuples with key-value pairs
 print("Items")
 print(person.items())
+
+# Keys returns a list of keys
 print("Keys")
 print(person.keys())
+
+# Values returns a list of values
 print("Values")
 print(person.values())
+
+# Copy returns a copy of the dictionary
